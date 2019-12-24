5a46a5ffeb8cbf4d6719212ee01270022e87ba09
mohit sanwal <mohits@dewsolutions.in>
2019-12-24 23:50:17 +0530

    fetching  data on server side with isomorphic unfetch

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,22 @@
 import Link from "next/link";
+import Head from "next/head";
+import Router from 'next/router';
+import NProgress from 'nprogress';
 
+
+Router.onRouteChangeStart = url => {
+    console.log(url);
+    NProgress.start();
+}
+
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done(); 
 
 export default ({ children, title }) => (
     <div className="root">
+        <Head>
+            <title> NextPortfolio</title>
+        </Head>
         <header>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,76 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import fetch from "isomorphic-unfetch";
 
-export default () => (
-    <Layout title="About">
-        <Link href="/">
-        <a>go to index</a>  
-        </Link>
-        <p> welcome to about file</p> 
-        <img src = "../static/images/img1.png"  alt="JavaScript" height="200" />
-    </Layout>
-)
+// export default () => (
+//     <Layout title="About">
+//         <Link href="/">
+//         <a>go to index</a>  
+//         </Link>
+//         <p> welcome to about file</p> 
+//         <img src = "../static/images/img1.png"  alt="JavaScript" height="200" />
+//     </Layout>
+// )
+
+
+export default class About extends React.Component {
+
+    // state = {
+    //     user: null
+    // }
+
+
+    // with server rendered app
+    // static getInitialProps() {
+    //     fetch('https://api.github.com/users/reedbarger')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log("data", data)
+    //     })
+    //     
+    //      /** Error: "About.getInitialProps()" should resolve to an object. But found "undefined" instead.
+    //       * to resolve this error we will return an object
+    //       */
+
+    //       return {user: "user"};
+    // }
+
+    /** another way to  */
+
+    static async getInitialProps() {
+        const res = await fetch('https://api.github.com/users/reedbarger')
+        const data = await res.json();
+        return {user: data};
+    }
+
+    // componentDidMount() {
+    //     fetch('https://api.github.com/users/reedbarger')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         this.setState({
+    //             user: data
+    //         });
+    //     })
+    /** as fetch is the browser api and we are rendering on server side so it will throw an error 
+    //      * that fetch is not defined */
+    // }
+
+    render() {
+        const { user } = this.props;
+        return(
+            <Layout title="About">
+                  {/* {JSON.stringify(this.props.user)} */}
+                <p> {user.name} </p>
+                <img 
+                src = {user.avatar_url}  
+                alt="JavaScript" 
+                height="200" />
+            </Layout>
+        )
+    }
+}
+
+
+// About.getInitialProps = () => {
+
+// }
